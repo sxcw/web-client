3867dc1a38687088de4d3e2580df69b11b6dbe27
Chuwei Lu <chuwei@chuweis-imac.lan>
2019-06-06 22:51:30 -0700

    Add dummy table

---
+++ src/components/EventsTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+class EventsTable extends React.Component {
+    render() {
+        return (
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Ship To</TableCell>
+                        <TableCell>Payment Method</TableCell>
+                        <TableCell align="right">Sale Amount</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    
+                </TableBody>
+            </Table>
+        );
+    }
+}
+
+export default EventsTable;
