5b9e3a61655793ab7faefcbdec3e818e10d71df4
Chuwei Lu <yuan329419@gmail.com>
2019-06-09 22:42:18 -0700

    Introduce React Hooks

--- src/components/EventsTable.js
+++ src/components/EventsTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -7,50 +7,57 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-class EventsTable extends React.Component {
-    constructor(props) {
-        super(props);
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
-        this.state = {
-            events: []
-        };
-    }
+const useStyles = makeStyles(theme => ({
+    fab: {
+        margin: theme.spacing(1),
+    },
+    extendedIcon: {
+        marginRight: theme.spacing(1),
+    },
+}));
 
-    componentDidMount() {
+function EventsTable(props) {
+    const [events, setEvents] = useState([]);
+    const classes = useStyles();
+
+    useEffect(() => {
         fetch('http://localhost:3000/api/events?userId=-1')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                this.setState({ events: data.Items });
+                setEvents(data.Items);
             });
-
-    }
-
-    render() {
-        const tableContent = this.state.events.map(event => {
-            return (<TableRow>
-                <TableCell>
-                    {event.eventName}
-                </TableCell>
-                <TableCell>
-                    {event.startTime}
-                </TableCell>
-            </TableRow>);
-        });
-        return (
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Start</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {tableContent}
-                </TableBody>
-            </Table>
-        );
-    }
+    }, []);
+    const tableContent = events.map(event => {
+        return (<TableRow>
+            <TableCell>
+                {event.eventName}
+            </TableCell>
+            <TableCell>
+                {event.startTime}
+            </TableCell>
+        </TableRow>);
+    });
+    return (
+        [<Table size="small">
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Start</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {tableContent}
+            </TableBody>
+        </Table>,
+        <Fab color="primary" aria-label="Add" className={classes.fab}>
+            <AddIcon />
+        </Fab>
+        ]
+    );
 }
 
 export default EventsTable;
